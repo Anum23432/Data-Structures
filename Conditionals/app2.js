30c79fbae7c2c0f76226bf0cad16b7de3cbf9c2d
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-06-12 19:57:52 +0500

    Add files via upload

---
+++ Conditionals/app2.js
@@ -0,0 +1,15 @@
+// You are given two numbers, num1 = 4 and num2 = 7.
+//  Add both and return true if both are equal to 11.
+//   Otherwise, return false, if they don’t equal 11.
+let num1 = 4;
+let num2 = 7;
+let a = num1 + num2;
+
+if (a = 11 ) {
+ console.log("true");
+}
+else {
+    console.log("false");
+}
+
+
