ead4c1da0fc7123b32efb6e4add30028ccf9c934
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-06-14 20:44:18 +0500

    If-else

---
+++ Conditionals/If-else/app2.js
@@ -0,0 +1,18 @@
+// Suppose a situation in which you are expected to show 
+// ‘less than 10’ if a number is less than 10. However,
+//  if it’s greater than 10, print ‘greater than 10’. Also, deal 
+//  with the case in which it’s equal to 10 with an appropriate message
+
+let y = 8;
+if(y < 10){
+    console.log("Less than 10.");
+}
+else if (y > 10){
+    console.log("Greater than 10.");
+}
+else if(y = 10){
+console.log("It is equal to 10.")
+}
+else{
+    ("unknown value.")
+}
---
+++ Conditionals/If-else/app4.js
@@ -0,0 +1,10 @@
+// Suppose you are given a number and you need to check 
+// whether it’s even or odd.How'd you do that?
+
+let v = 8;
+if (v % 2 == 0) {
+    console.log("It is even number.");
+}
+else{
+    ("It is an odd number.");
+}
