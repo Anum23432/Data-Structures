27387dbeaae0821a20703e772d8cecda3f7d49f7
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-06-15 19:29:20 +0500

    for-loop

---
+++ Loops/for-loop/app4.js
@@ -0,0 +1,9 @@
+// Consider you are given an array [2,0,1,6,45,89]. 
+// You need to find the largest element in the array.
+//  How’d you do that?
+
+let array = [2,0,1,6,45,89];
+let x = Math.max(...array); 
+ 
+    console.log(x);
+    // printing 89 in console.
---
+++ Loops/for-loop/app5.js
@@ -0,0 +1,8 @@
+// Consider the above array again and 
+// find the smallest element in the array.
+
+let array =  [2,0,1,6,45,89];
+let a = Math.min(...array);
+console.log(a);
+
+// printing 0 in console
